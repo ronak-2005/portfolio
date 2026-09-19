aab4a93cd5702ccc9093dfe92aedda838a8fe82a
Ronak <chaturvedironak484@gmail.com>
2026-09-19 14:38:06 +0530

    Update page.tsx

--- src/app/resume-view/page.tsx
+++ src/app/resume-view/page.tsx
@@ -4,7 +4,7 @@ export default function ResumeView() {
   return (
     <div style={{ textAlign: "center", margin: "50px", fontFamily: "sans-serif" }}>
       <iframe
-        src="/resume.pdf"
+        src="/ronak.pdf"
         style={{ width: "80%", height: "80vh", border: "1px solid #ccc", borderRadius: "8px" }}
       ></iframe>
       <br />
